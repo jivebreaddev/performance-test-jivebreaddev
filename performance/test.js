084b7fb53fff6d836799b25986047943509567e3
Jivebread <spark@troy.edu>
2024-08-11 23:21:27 +0900

    feature(performance-test): 성능 테스트를 진행합니다.

--- performance/test.js
+++ performance/test.js
@@ -14,7 +14,7 @@ export default function () {
         email: "heallo",
         phone: "123-123-1231"
     });
-    let response = http.get('http://43.201.169.49:8080/api/v1/user');
+    let response = http.post('http://43.201.169.49:8080/api/v1/user');
 
     // 응답 확인
     check(response, {
