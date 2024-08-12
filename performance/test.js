f49f659b8427b3a9e23e9759c3b959d57bbc782c
Jivebread <spark@troy.edu>
2024-08-12 20:14:49 +0900

    feature(performance-test): 성능 테스트를 진행합니다.

--- performance/test.js
+++ performance/test.js
@@ -19,12 +19,12 @@ export default function () {
                 'Content-Type': 'application/json',
             },
         };
-    let response = http.post('http://43.201.169.49:8080/api/v1/user/', body, params);
+    let response = http.post('http://${__ENV.MY_HOSTNAME}:8080/api/v1/user/', body, params);
 
     // 응답 확인
     check(response, {
         'is status 200': (r) => r.status === 200,
     }); //     command: run /test.js -e MY_HOSTNAME=43.201.169.49 --out json=test.json --out influxdb=http://localhost:8086/myk6db
 //docker-compose exec k6-client run /test.js --out json=test.json --out influxdb=http://localhost:8086/myk6db
-    sleep(0.1); // 유저당 8 반복/s  -> 8 * 500 * 60 * 10번 = 240_000번
+    sleep(0.5); // 유저당 8 반복/s  -> 8 * 500 * 60 * 10번 = 240_000번
 }
